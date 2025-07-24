44e47b04a8c1a0b6739a6006bd819a626fc3eb1e
LENOVO <swalihmkd00@gmail.com>
2025-07-24 11:42:05 +0530

    initial commit

--- lib/dashboardPages/stylesPages.dart
+++ lib/dashboardPages/stylesPages.dart
@@ -665,7 +665,7 @@ scrollDirection: Axis.horizontal,
                                                               ),
                                                               checkColor: Colors.black,
                                                               activeColor: Colors.orange,
-                                                              value: value.checkindex.contains(index)?true:false,
+                                                              value: value.checkIndex.contains(index)?true:false,
                                                               onChanged: (selected) {
                                                                 value.stylePageCheck(index);
                                                               },
